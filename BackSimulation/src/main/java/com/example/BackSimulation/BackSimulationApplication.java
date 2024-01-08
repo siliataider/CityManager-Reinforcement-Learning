438c84faccde148cdc02c83b121b05e7b88d9ed5
Jad GHANDOUR <jad.ghandour@tpopt08.cpe.lan>
2024-01-08 17:49:17 +0100

    how does maven work?

---
+++ BackSimulation/src/main/java/com/example/BackSimulation/BackSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackSimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSimulationApplication.class, args);
+	}
+
+}
